8d726e50cd5ffdc1f4efa72f11e84788ce9e6854
0xygen15 <felix.ete.dev@proton.me>
2024-03-29 14:47:13 +0200

    attemt to postgres

--- database/create_db.py
+++ database/create_db.py
@@ -1,10 +1,34 @@
-import sqlite3
+# import sqlite3
+#
+# # filename to form database
+# file = "users.db"
+#
+# try:
+#     conn = sqlite3.connect(file)
+#     print("Database Sqlite3.db formed.")
+# except:
+#     print("Database Sqlite3.db not formed.")
 
-# filename to form database
-file = "users.db"
+import psycopg2
 
 try:
-    conn = sqlite3.connect(file)
-    print("Database Sqlite3.db formed.")
-except:
-    print("Database Sqlite3.db not formed.")
+    # connect to exist database
+    connection = psycopg2.connect(
+        host="172.19.0.2",
+        user="sintia",
+        password="example",
+        database="users",
+        port="5432"
+    )
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+
+        print(f"Server version: {cursor.fetchone()}")
+
+
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
