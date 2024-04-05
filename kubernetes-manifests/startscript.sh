f49b99fdb1d77144647d01a69532db2ce81983e9
0xygen15 <felix.ete.dev@proton.me>
2024-04-05 16:21:36 +0300

    minor changes in startscript

--- kubernetes-manifests/startscript.sh
+++ kubernetes-manifests/startscript.sh
@@ -2,11 +2,13 @@
 minikube start --driver="docker"
 minikube addons enable ingress
 
-#prepare storage
+#prepare storage (in order for Stateful containers to be working)
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 #create pods
-kubectl apply -f "*.yaml"
-
-echo ""
+cd kubernetes-manifests
+kubectl apply -f ingress.yaml
+kubectl apply -f postgres-deployment.yaml
+kubectl apply -f pgadmin-deployment.yaml
+kubectl apply -f sintia-app-deployment.yaml
